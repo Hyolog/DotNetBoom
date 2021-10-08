7f0e24eccf6a27117d3429af6ce884995cbb4972
dhkang <soejdtodxn@naver.com>
2021-10-09 01:26:39 +0900

    검색 기능 추가

--- DotNetBoom/Models/Company.cs
+++ DotNetBoom/Models/Company.cs
@@ -6,12 +6,24 @@ namespace DotNetBoom.Models
     public class Company
     {
         public int ID { get; set; }
+
+        [Display(Name = "회사명")]
         public string Name { get; set; }
+
+        [Display(Name = "설립일")]
         [DataType(DataType.Date)]
         public DateTime FoundationDate { get; set; }
+
+        [Display(Name = "위치")]
         public string Location { get; set; }
+
+        [Display(Name = "산업군")]
         public string BusinessGroup { get; set; }
+
+        [Display(Name = "기업형태")]
         public string Type { get; set; }
+
+        [Display(Name = "사원수")]
         public int NumberOfEmployees { get; set; }
     }
 }
--- DotNetBoom/Pages/Companies/Index.cshtml.cs
+++ DotNetBoom/Pages/Companies/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using DotNetBoom.Data;
 using DotNetBoom.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DotNetBoom.Pages.Companies
 {
@@ -21,9 +22,39 @@ namespace DotNetBoom.Pages.Companies
 
         public IList<Company> Company { get;set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        public SelectList BusinessGroups { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string CompanyBusinessGroup { get; set; }
+
         public async Task OnGetAsync()
         {
-            Company = await _context.Company.ToListAsync();
+            IQueryable<string> businessGroupQuery = from c in _context.Company
+                                           orderby c.BusinessGroup
+                                           select c.BusinessGroup;
+
+            var companies = from c in _context.Company
+                            select c;
+
+            if (!string.IsNullOrEmpty(SearchString))
+                companies = companies.Where(d => d.Name.Contains(SearchString));
+
+            if (!string.IsNullOrEmpty(CompanyBusinessGroup))
+                companies = companies.Where(d => d.BusinessGroup.Contains(CompanyBusinessGroup));
+
+            BusinessGroups = new SelectList(await businessGroupQuery.Distinct().ToListAsync());
+            Company = await companies.ToListAsync(); 
+
+            //var companies = from c in _context.Company
+            //                select c;
+
+            //if (!string.IsNullOrEmpty(SearchString))
+            //    companies = companies.Where(d => d.Name.Contains(SearchString));
+
+            //Company = await companies.ToListAsync();
         }
     }
 }
